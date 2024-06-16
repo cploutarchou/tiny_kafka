930d6caefe4bd7b145a5fcb874da21975df7fd7d
Christos Ploutarchou <cploutarchou@gmail.com>
2024-06-16 23:45:45 +0300

    create custom producer.rs

--- src/consumer.rs
+++ src/consumer.rs
@@ -1,221 +1,221 @@
-use log::{error, info};
-use rdkafka::config::ClientConfig;
-use rdkafka::consumer::{Consumer, StreamConsumer};
-use rdkafka::Message as KafkaMessage;
-use serde::Deserialize;
-use std::collections::HashMap;
-use tokio_stream::StreamExt;
-
-/// Represents a message received from Kafka with its key and value.
-#[derive(Deserialize)]
-pub struct ReceivedMessage {
-    pub key: String,
-    pub value: String,
-}
-
-/// Provides functionality to consume messages from a Kafka topic.
-///
-/// The `KafkaConsumer` allows you to connect to a Kafka broker, subscribe to a topic,
-/// and consume messages from that topic. The consumer can be reconfigured with custom settings
-/// as needed.
-///
-/// # Examples
-///
-/// Basic usage:
-///
-/// ```no_run
-/// use std::collections::HashMap;
-/// use tiny_kafka::consumer::KafkaConsumer;
-/// let mut consumer = KafkaConsumer::new("localhost:9092", "my_group", "my_topic");
-///
-/// // Consume messages in a loop
-/// tokio::spawn(async move {
-///     loop {
-///         if let Some(msg) = consumer.poll().await {
-///             println!("Received: {} -> {}", msg.key, msg.value);
-///         }
-///     }
-/// });
-/// ```
-pub struct KafkaConsumer {
-    consumer: StreamConsumer,
-}
-impl KafkaConsumer {
-    /// Creates a new instance of the Kafka consumer.
-    ///
-    /// Initializes the consumer with the given broker addresses, group ID, and topic name.
-    /// The consumer starts reading messages from the specified topic once it's created.
-    ///
-    /// # Arguments
-    ///
-    /// * `brokers`: A comma-separated list of broker addresses.
-    /// * `group_id`: The ID of the consumer group.
-    /// * `topic_name`: The name of the Kafka topic to which the consumer should subscribe.
-    ///
-    /// # Panics
-    ///
-    /// * If the consumer creation fails.
-    /// * If the consumer cannot subscribe to the specified topic.
-    ///
-    /// # Examples
-    ///
-    /// Basic usage:
-    ///
-    /// ```no_run
-    /// use tiny_kafka::consumer::KafkaConsumer;
-    /// let consumer = KafkaConsumer::new("localhost:9092", "my_group", "my_topic");
-    /// ```
-    pub fn new(brokers: &str, group_id: &str, topic_name: &str) -> Self {
-        let consumer: StreamConsumer = ClientConfig::new()
-            .set("bootstrap.servers", brokers)
-            .set("group.id", group_id)
-            .create()
-            .expect("Consumer creation failed");
-
-        consumer
-            .subscribe(&[topic_name])
-            .expect("Can't subscribe to specified topic");
-
-        KafkaConsumer { consumer }
-    }
-    /// Polls for a message from the subscribed Kafka topic.
-    ///
-    /// Checks for a new message in the Kafka topic. If a message is found, it's deserialized
-    /// and returned as an instance of `ReceivedMessage`. If no message is found or an error occurs,
-    /// it returns `None`.
-    ///
-    /// This function asynchronously polls the Kafka topic and returns once a message is found or
-    /// if there's an error. In the latter case, the error is logged.
-    ///
-    /// # Returns
-    ///
-    /// An `Option` containing a `ReceivedMessage` if a message is received, or `None` otherwise.
-    ///
-    /// # Examples
-    ///
-    /// Basic usage:
-    ///
-    /// ```no_run
-    /// use tiny_kafka::consumer::KafkaConsumer;
-    /// tokio::spawn(async move {
-    ///     let consumer = KafkaConsumer::new("localhost:9092", "my_group", "my_topic");
-    ///     if let Some(msg) = consumer.poll().await {
-    ///         println!("Received: {} -> {}", msg.key, msg.value);
-    ///     }
-    /// });
-    /// ```
-    pub async fn poll(&self) -> Option<ReceivedMessage> {
-        if let Some(result) = self.consumer.stream().next().await {
-            match result {
-                Ok(kafka_message) => {
-                    let key = match kafka_message.key() {
-                        Some(k) => String::from_utf8_lossy(k).to_string(),
-                        None => String::from(""),
-                    };
-
-                    let value = match kafka_message.payload_view::<str>() {
-                        Some(Ok(v)) => v.to_owned(),
-                        Some(Err(e)) => {
-                            error!("Error deserializing message payload: {:?}", e);
-                            return None;
-                        }
-                        None => return None,
-                    };
-
-                    info!("Received message with key {}: {}", key, value);
-                    Some(ReceivedMessage { key, value })
-                }
-                Err(e) => {
-                    error!("Error while receiving message: {:?}", e);
-                    None
-                }
-            }
-        } else {
-            None
-        }
-    }
-    /// Sets custom configurations for the Kafka consumer and recreates it with the given configurations.
-    ///
-    /// This method allows you to modify the Kafka consumer configurations dynamically. After setting the
-    /// desired configurations, the existing consumer instance is replaced with a new one using the updated
-    /// configurations. The consumer then subscribes to the specified topic.
-    ///
-    /// # Arguments
-    ///
-    /// * `brokers`: A comma-separated list of broker addresses. This is used to specify to which brokers the consumer should connect.
-    /// * `group_id`: The ID of the consumer group. It is used to identify a group of consumer processes that jointly consume messages from one or multiple Kafka topics.
-    /// * `topic_name`: The name of the Kafka topic to which the consumer should subscribe.
-    /// * `configurations`: A hashmap containing additional configurations you want to set for the Kafka consumer. Each entry in the hashmap represents a key-value configuration pair.
-    ///
-    /// # Panics
-    ///
-    /// This method will panic if:
-    ///
-    /// * The consumer creation fails with the given configurations.
-    /// * The consumer fails to subscribe to the specified topic.
-    ///
-    /// # Example
-    ///
-    /// ```no_run
-    /// use std::collections::HashMap;
-    /// use tiny_kafka::consumer::KafkaConsumer;
-    /// let mut consumer = KafkaConsumer::new("localhost:9092", "my_group", "my_topic");
-    /// let mut new_configs = HashMap::new();
-    /// new_configs.insert("auto.offset.reset".to_string(), "earliest".to_string());
-    /// consumer.set_client_config("localhost:9092", "my_group", "my_topic", new_configs);
-    /// ```
-    pub fn set_client_config(
-        &mut self,
-        brokers: &str,
-        group_id: &str,
-        topic_name: &str,
-        configurations: HashMap<String, String>,
-    ) {
-        let mut client_config = ClientConfig::new();
-        client_config.set("bootstrap.servers", brokers);
-        client_config.set("group.id", group_id);
-
-        for (key, value) in &configurations {
-            client_config.set(key, value);
-        }
-
-        self.consumer = client_config
-            .create::<StreamConsumer>()
-            .expect("Failed to create new consumer with updated configurations");
-        self.consumer
-            .subscribe(&[topic_name])
-            .expect("Can't subscribe to specified topic");
-    }
-}
-/// Unit tests for the KafkaConsumer functionality.
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use log::LevelFilter;
-    use simple_logger::SimpleLogger;
-
-    /// Tests the functionality of the KafkaConsumer by initializing it, polling for a message,
-    /// and logging the received message.
-    #[tokio::test]
-    async fn test_kafka_consumer() {
-        // Initialize the logger for the test
-        SimpleLogger::new()
-            .with_level(LevelFilter::Info)
-            .init()
-            .unwrap();
-
-        let brokers = "localhost:9092";
-        let group_id = "test-group".to_string();
-        let topic = "test-topic";
-        let consumer = KafkaConsumer::new(brokers, group_id.as_str(), topic);
-        loop {
-            if let Some(msg) = consumer.poll().await {
-                info!(
-                    "Consumed message with key: {} and value: {}",
-                    msg.key, msg.value
-                );
-            }
-            return;
-        }
-    }
-}
+// use log::{error, info};
+// use rdkafka::config::ClientConfig;
+// use rdkafka::consumer::{Consumer, StreamConsumer};
+// use rdkafka::Message as KafkaMessage;
+// use serde::Deserialize;
+// use std::collections::HashMap;
+// use tokio_stream::StreamExt;
+//
+// /// Represents a message received from Kafka with its key and value.
+// #[derive(Deserialize)]
+// pub struct ReceivedMessage {
+//     pub key: String,
+//     pub value: String,
+// }
+//
+// /// Provides functionality to consume messages from a Kafka topic.
+// ///
+// /// The `KafkaConsumer` allows you to connect to a Kafka broker, subscribe to a topic,
+// /// and consume messages from that topic. The consumer can be reconfigured with custom settings
+// /// as needed.
+// ///
+// /// # Examples
+// ///
+// /// Basic usage:
+// ///
+// /// ```no_run
+// /// use std::collections::HashMap;
+// /// use tiny_kafka::consumer::KafkaConsumer;
+// /// let mut consumer = KafkaConsumer::new("localhost:9092", "my_group", "my_topic");
+// ///
+// /// // Consume messages in a loop
+// /// tokio::spawn(async move {
+// ///     loop {
+// ///         if let Some(msg) = consumer.poll().await {
+// ///             println!("Received: {} -> {}", msg.key, msg.value);
+// ///         }
+// ///     }
+// /// });
+// /// ```
+// pub struct KafkaConsumer {
+//     consumer: StreamConsumer,
+// }
+// impl KafkaConsumer {
+//     /// Creates a new instance of the Kafka consumer.
+//     ///
+//     /// Initializes the consumer with the given broker addresses, group ID, and topic name.
+//     /// The consumer starts reading messages from the specified topic once it's created.
+//     ///
+//     /// # Arguments
+//     ///
+//     /// * `brokers`: A comma-separated list of broker addresses.
+//     /// * `group_id`: The ID of the consumer group.
+//     /// * `topic_name`: The name of the Kafka topic to which the consumer should subscribe.
+//     ///
+//     /// # Panics
+//     ///
+//     /// * If the consumer creation fails.
+//     /// * If the consumer cannot subscribe to the specified topic.
+//     ///
+//     /// # Examples
+//     ///
+//     /// Basic usage:
+//     ///
+//     /// ```no_run
+//     /// use tiny_kafka::consumer::KafkaConsumer;
+//     /// let consumer = KafkaConsumer::new("localhost:9092", "my_group", "my_topic");
+//     /// ```
+//     pub fn new(brokers: &str, group_id: &str, topic_name: &str) -> Self {
+//         let consumer: StreamConsumer = ClientConfig::new()
+//             .set("bootstrap.servers", brokers)
+//             .set("group.id", group_id)
+//             .create()
+//             .expect("Consumer creation failed");
+//
+//         consumer
+//             .subscribe(&[topic_name])
+//             .expect("Can't subscribe to specified topic");
+//
+//         KafkaConsumer { consumer }
+//     }
+//     /// Polls for a message from the subscribed Kafka topic.
+//     ///
+//     /// Checks for a new message in the Kafka topic. If a message is found, it's deserialized
+//     /// and returned as an instance of `ReceivedMessage`. If no message is found or an error occurs,
+//     /// it returns `None`.
+//     ///
+//     /// This function asynchronously polls the Kafka topic and returns once a message is found or
+//     /// if there's an error. In the latter case, the error is logged.
+//     ///
+//     /// # Returns
+//     ///
+//     /// An `Option` containing a `ReceivedMessage` if a message is received, or `None` otherwise.
+//     ///
+//     /// # Examples
+//     ///
+//     /// Basic usage:
+//     ///
+//     /// ```no_run
+//     /// use tiny_kafka::consumer::KafkaConsumer;
+//     /// tokio::spawn(async move {
+//     ///     let consumer = KafkaConsumer::new("localhost:9092", "my_group", "my_topic");
+//     ///     if let Some(msg) = consumer.poll().await {
+//     ///         println!("Received: {} -> {}", msg.key, msg.value);
+//     ///     }
+//     /// });
+//     /// ```
+//     pub async fn poll(&self) -> Option<ReceivedMessage> {
+//         if let Some(result) = self.consumer.stream().next().await {
+//             match result {
+//                 Ok(kafka_message) => {
+//                     let key = match kafka_message.key() {
+//                         Some(k) => String::from_utf8_lossy(k).to_string(),
+//                         None => String::from(""),
+//                     };
+//
+//                     let value = match kafka_message.payload_view::<str>() {
+//                         Some(Ok(v)) => v.to_owned(),
+//                         Some(Err(e)) => {
+//                             error!("Error deserializing message payload: {:?}", e);
+//                             return None;
+//                         }
+//                         None => return None,
+//                     };
+//
+//                     info!("Received message with key {}: {}", key, value);
+//                     Some(ReceivedMessage { key, value })
+//                 }
+//                 Err(e) => {
+//                     error!("Error while receiving message: {:?}", e);
+//                     None
+//                 }
+//             }
+//         } else {
+//             None
+//         }
+//     }
+//     /// Sets custom configurations for the Kafka consumer and recreates it with the given configurations.
+//     ///
+//     /// This method allows you to modify the Kafka consumer configurations dynamically. After setting the
+//     /// desired configurations, the existing consumer instance is replaced with a new one using the updated
+//     /// configurations. The consumer then subscribes to the specified topic.
+//     ///
+//     /// # Arguments
+//     ///
+//     /// * `brokers`: A comma-separated list of broker addresses. This is used to specify to which brokers the consumer should connect.
+//     /// * `group_id`: The ID of the consumer group. It is used to identify a group of consumer processes that jointly consume messages from one or multiple Kafka topics.
+//     /// * `topic_name`: The name of the Kafka topic to which the consumer should subscribe.
+//     /// * `configurations`: A hashmap containing additional configurations you want to set for the Kafka consumer. Each entry in the hashmap represents a key-value configuration pair.
+//     ///
+//     /// # Panics
+//     ///
+//     /// This method will panic if:
+//     ///
+//     /// * The consumer creation fails with the given configurations.
+//     /// * The consumer fails to subscribe to the specified topic.
+//     ///
+//     /// # Example
+//     ///
+//     /// ```no_run
+//     /// use std::collections::HashMap;
+//     /// use tiny_kafka::consumer::KafkaConsumer;
+//     /// let mut consumer = KafkaConsumer::new("localhost:9092", "my_group", "my_topic");
+//     /// let mut new_configs = HashMap::new();
+//     /// new_configs.insert("auto.offset.reset".to_string(), "earliest".to_string());
+//     /// consumer.set_client_config("localhost:9092", "my_group", "my_topic", new_configs);
+//     /// ```
+//     pub fn set_client_config(
+//         &mut self,
+//         brokers: &str,
+//         group_id: &str,
+//         topic_name: &str,
+//         configurations: HashMap<String, String>,
+//     ) {
+//         let mut client_config = ClientConfig::new();
+//         client_config.set("bootstrap.servers", brokers);
+//         client_config.set("group.id", group_id);
+//
+//         for (key, value) in &configurations {
+//             client_config.set(key, value);
+//         }
+//
+//         self.consumer = client_config
+//             .create::<StreamConsumer>()
+//             .expect("Failed to create new consumer with updated configurations");
+//         self.consumer
+//             .subscribe(&[topic_name])
+//             .expect("Can't subscribe to specified topic");
+//     }
+// }
+// /// Unit tests for the KafkaConsumer functionality.
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use log::LevelFilter;
+//     use simple_logger::SimpleLogger;
+//
+//     /// Tests the functionality of the KafkaConsumer by initializing it, polling for a message,
+//     /// and logging the received message.
+//     #[tokio::test]
+//     async fn test_kafka_consumer() {
+//         // Initialize the logger for the test
+//         SimpleLogger::new()
+//             .with_level(LevelFilter::Info)
+//             .init()
+//             .unwrap();
+//
+//         let brokers = "localhost:9092";
+//         let group_id = "test-group".to_string();
+//         let topic = "test-topic";
+//         let consumer = KafkaConsumer::new(brokers, group_id.as_str(), topic);
+//         loop {
+//             if let Some(msg) = consumer.poll().await {
+//                 info!(
+//                     "Consumed message with key: {} and value: {}",
+//                     msg.key, msg.value
+//                 );
+//             }
+//             return;
+//         }
+//     }
+// }
--- src/producer.rs
+++ src/producer.rs
@@ -1,196 +1,89 @@
-// Import necessary libraries and modules
-use log::{error, info};
-use rdkafka::config::ClientConfig;
-use rdkafka::producer::{FutureProducer, FutureRecord};
-use serde::Serialize;
-use serde_json::to_string;
-use std::collections::HashMap;
-use std::sync::Arc;
-use std::time::Duration;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+use bytes::BytesMut;
+use tokio::test; // Ensure to import the macro correctly
 
-/// Represents a message with a key and value to be sent to Kafka.
-#[derive(Serialize)]
-pub struct Message {
-    pub key: String,
-    pub value: String,
-}
-
-impl Message {
-    /// Create a new message instance with the specified key and value.
-    ///
-    /// # Arguments
-    /// * `key`: The message key.
-    /// * `value`: The message value.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use tiny_kafka::producer::Message;
-    /// let msg = Message::new("key1", "value1");
-    /// assert_eq!(msg.key, "key1");
-    /// assert_eq!(msg.value, "value1");
-    /// ```
-    pub fn new(key: &str, value: &str) -> Self {
-        Self {
-            key: key.to_string(),
-            value: value.to_string(),
-        }
-    }
-}
-
-/// KafkaProducer provides a higher-level interface for producing messages to Kafka.
-pub struct KafkaProducer {
-    producer: Arc<FutureProducer>,
+struct KafkaProducer {
+    broker_address: String,
+    stream: Option<TcpStream>,
 }
 
 impl KafkaProducer {
-    /// Initialize a new KafkaProducer with the specified broker and optional configurations.
-    ///
-    /// # Arguments
-    /// * `brokers`: A comma-separated list of broker addresses.
-    /// * `configurations`: Optional custom configurations as a key-value hashmap.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// // Initialize a KafkaProducer without any custom configurations
-    /// use std::collections::HashMap;
-    /// use tiny_kafka::producer::KafkaProducer;
-    /// let producer = KafkaProducer::new("localhost:9092", None);
-    ///
-    /// // With custom configurations
-    /// let mut configs = HashMap::new();
-    /// configs.insert("max.in.flight.requests.per.connection", "5");
-    /// let producer_with_configs = KafkaProducer::new("localhost:9092", Some(configs));
-    /// ```
-    pub fn new(brokers: &str, configurations: Option<HashMap<&str, &str>>) -> Self {
-        let mut client_config = ClientConfig::new();
-        client_config.set("bootstrap.servers", brokers);
-
-        // If additional configurations are provided, apply them.
-        if let Some(configs) = configurations {
-            for (key, value) in configs {
-                client_config.set(key, value);
-            }
-        }
-
-        let producer = Arc::new(
-            client_config
-                .set("message.timeout.ms", "5000")
-                .create()
-                .expect("Producer creation error"),
-        );
-
-        KafkaProducer { producer }
+    /// Create a new KafkaProducer instance.
+    pub async fn new(broker_address: String) -> io::Result<Self> {
+        let mut producer = KafkaProducer {
+            broker_address,
+            stream: None,
+        };
+        producer.connect().await?;
+        Ok(producer)
     }
 
-    /// Send a message to the specified Kafka topic asynchronously.
-    ///
-    /// # Arguments
-    /// * `topic_name`: The name of the Kafka topic.
-    /// * `message`: The message to send.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use tiny_kafka::producer::{KafkaProducer, Message};
-    /// let producer = KafkaProducer::new("localhost:9092", None);
-    /// let msg = Message::new("key1", "value1");
-    ///
-    /// // Using async runtime for demonstration, replace as needed
-    /// let runtime = tokio::runtime::Runtime::new().unwrap();
-    /// runtime.block_on(async {
-    ///     producer.send_message("my-topic", msg).await;
-    /// });
-    /// ```
-    pub async fn send_message(&self, topic_name: &str, message: Message) {
-        let json_payload = to_string(&message.value).expect("Failed to serialize message");
-
-        let delivery_status = self
-            .producer
-            .send(
-                FutureRecord::to(topic_name)
-                    .payload(&json_payload)
-                    .key(&message.key),
-                Duration::from_secs(0),
-            )
-            .await;
-
-        // Log the status of the message delivery.
-        match delivery_status {
-            Ok((partition, offset)) => {
-                info!(
-                    "Message with key {} delivered to partition {} at offset {}",
-                    message.key, partition, offset
-                );
-            }
-            Err((error, _message)) => {
-                error!(
-                    "Failed to deliver message with key {}: {}",
-                    message.key, error
-                );
-            }
-        }
+    /// Establish a connection to the Kafka broker.
+    async fn connect(&mut self) -> io::Result<()> {
+        let stream = TcpStream::connect(&self.broker_address).await?;
+        self.stream = Some(stream);
+        Ok(())
     }
 
-    /// Set custom configurations for the Kafka producer.
-    ///
-    /// # Arguments
-    /// * `configurations`: A hashmap of key-value pairs to set as configurations.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use std::collections::HashMap;
-    /// use tiny_kafka::producer::KafkaProducer;
-    /// let mut producer = KafkaProducer::new("localhost:9092", None);
-    ///
-    /// // Update producer configurations
-    /// let mut new_configs = HashMap::new();
-    /// new_configs.insert("acks", "all");
-    /// producer.set_client_config(new_configs);
-    /// ```
-    pub fn set_client_config(&mut self, configurations: HashMap<&str, &str>) {
-        let mut client_config = ClientConfig::new();
-
-        for (key, value) in configurations {
-            client_config.set(key, value);
+    /// Send a message to the specified topic and partition.
+    pub async fn send_message(&mut self, topic: &str, partition: i32, message: &str) -> io::Result<()> {
+        let request = self.create_produce_request(topic, partition, message);
+        if let Some(ref mut stream) = self.stream {
+            stream.write_all(&request).await?;
+            stream.flush().await?;
+            self.receive_response().await?;
+        } else {
+            return Err(io::Error::new(io::ErrorKind::NotConnected, "Not connected to Kafka broker"));
         }
+        Ok(())
+    }
 
-        self.producer = Arc::new(
-            client_config
-                .create()
-                .expect("Failed to set new configuration"),
-        );
+    /// Create a Produce request for Kafka.
+    fn create_produce_request(&self, topic: &str, partition: i32, message: &str) -> BytesMut {
+        let mut buffer = BytesMut::new();
+        buffer.extend_from_slice(topic.as_bytes());
+        buffer.extend_from_slice(&partition.to_be_bytes());
+        buffer.extend_from_slice(message.as_bytes());
+        buffer
+    }
+
+    /// Receive and handle the response from Kafka.
+    async fn receive_response(&mut self) -> io::Result<()> {
+        let mut response = vec![0; 1024]; // Fixed size for demonstration
+        if let Some(ref mut stream) = self.stream {
+            stream.read_exact(&mut response).await?;
+            println!("Received response: {:?}", response);
+        }
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use log::LevelFilter;
-    use simple_logger::SimpleLogger;
+    use tokio::test;
 
     #[tokio::test]
-    async fn test_kafka_producer() {
-        // Initialize the logger for the test
-        SimpleLogger::new()
-            .with_level(LevelFilter::Info)
-            .init()
-            .unwrap();
+    async fn test_new_producer_success() {
+        let addr = "127.0.0.1:9092".to_string();
+        let producer = KafkaProducer::new(addr).await;
+        assert!(producer.is_ok());
+    }
 
-        // Define the Kafka configurations
-        let brokers = "localhost:9092";
+    #[tokio::test]
+    async fn test_send_message() {
+        let client_stream = TcpStream::connect("127.0.0.1:9092").await.expect("Failed to connect for test");
+        let mut producer = KafkaProducer {
+            broker_address: "127.0.0.1:9092".to_string(),
+            stream: Some(client_stream),
+        };
+
         let topic = "test-topic";
-        let producer = KafkaProducer::new(brokers, Option::None);
+        let partition = 0;
+        let message = "Hello Kafka!";
 
-        for i in 0..100 {
-            let key = format!("test_key_{}", i);
-            let value = format!("test_value_{}", i);
-            let message = Message::new(&key, &value);
-
-            producer.send_message(topic, message).await;
-            tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
-        }
+        let result = producer.send_message(topic, partition, message).await;
+        assert!(result.is_ok());
     }
 }
